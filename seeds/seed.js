d75f1d6fbcab0c1f09002551b4c887e042cfc0ee
HenniePenny <h.kressner@hotmail.de>
2022-09-25 17:07:14 +0200

    add seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require("../config/connection");
+const { User, BlogPost, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const blogpostData = require("./blogpostData.json");
+const commentData = require("./commentData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
