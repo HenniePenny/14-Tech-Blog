4acc4b2f02e8ddfb8baed610b8be92071065aa73
HenniePenny <h.kressner@hotmail.de>
2022-09-25 11:57:49 +0200

    add blogpost model

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,2 +1,45 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    blogger_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+        unique: false,
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blogpost",
+  }
+);
+
+module.exports = BlogPost;
