1b84cce0c1bed0dbbb017e1b7408354e60b76eae
HenniePenny <h.kressner@hotmail.de>
2022-09-26 09:36:53 +0200

    add api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const blogPostRoutes = require("./blogPostRoutes");
-const commentRoutes = require("./commentRoutes");
+
+router.use("/users", userRoutes);
+router.use("/blogs", blogPostRoutes);
+
+module.exports = router;
