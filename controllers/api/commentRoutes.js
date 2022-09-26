69c4ab1a6ce14d849b6d6413b024adc51506a57f
HenniePenny <h.kressner@hotmail.de>
2022-09-26 15:54:32 +0200

    add comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,19 @@
-//leave a comment on a blogpost
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//leave a comment on a blog post (post)
+router.post("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = router;
