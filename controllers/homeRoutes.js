9bd4b234d2272361942a4cad4ed2005520b4fc89
HenniePenny <h.kressner@hotmail.de>
2022-09-26 15:00:30 +0200

    add layout (comments) for home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+//get all blog posts for the homepage (what can be viewed without login)
+
+//dashboard routes here?
+
+//login/sign up
+
+//logout
